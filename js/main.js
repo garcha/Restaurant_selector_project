3c43e57c46ecd0090003c3882c5b3f31545c917e
blaze2236 <jaspreet@garcha.com>
2013-09-02 00:27:43 -0700

    final version

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ var terms = 'Mexican';
 var near = 'Santa Monica';
 
 var accessor = {
-  consumerSecret: auth.consumerSecret,
-  tokenSecret: auth.accessTokenSecret
+    consumerSecret: auth.consumerSecret,
+    tokenSecret: auth.accessTokenSecret
 };
 
 parameters = [];
@@ -30,181 +30,137 @@ parameters.push(['oauth_token', auth.accessToken]);
 parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
 
 var message = { 
-  'action': 'http://api.yelp.com/v2/search',
-  'method': 'GET',
-  'parameters': parameters 
+    'action': 'http://api.yelp.com/v2/search',
+    'method': 'GET',
+    'parameters': parameters 
 };
 
 OAuth.setTimestampAndNonce(message);
 OAuth.SignatureMethod.sign(message, accessor);
 
-
-// $(event.keycode === 13){
-//   $(this).
-// }
-
-//     $("#id_of_textbox").keyup(function(event){
-//     if(event.keyCode == 13){
-//         $("#id_of_button").click();
-//     }
-// });
-
+//submit button and runs the getData function
 $(function(){
   // getData();
 
-
-
   $('#submit').on("click", function() {
-    // get data from inputs
+      // get data from inputs
 
-    terms = $('#terms').val();
-    near = $('#near').val();
-    // replace data in parameters
+      terms = $('#terms').val();
+      near = $('#near').val();
+      
+      // replace data in parameters
+      parameters.shift();
+      parameters.shift();
+      parameters.unshift(['location', near]);
+      parameters.unshift(['term', terms]);
 
+      //new timestamp for API
+      OAuth.setTimestampAndNonce(message);
+      OAuth.SignatureMethod.sign(message, accessor);
 
-    parameters.shift();
-    parameters.shift();
-    parameters.unshift(['location', near]);
-    parameters.unshift(['term', terms]);
-
-
-    OAuth.setTimestampAndNonce(message);
-    OAuth.SignatureMethod.sign(message, accessor);
-
-    // getData
-    getData();
-    return false
-
-
-
+      // getData
+      getData();
+      return false
 
   });
 });
-
+//getData from yelp, and write function are run
 function getData() {
   var parameterMap = OAuth.getParameterMap(message.parameters);
   parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
   $.ajax({
-    'url': message.action,
-    'data': parameterMap,
-    'cache': true,
-    'dataType': 'jsonp',
-    'jsonpCallback': 'cb',
-    'success': function(data, textStats, XMLHttpRequest) {
-      gdata = data;
-      console.log(data);
-      //var output = prettyPrint(data);
-      //$("body").append(output);
-      
-      businesses = [];
+      'url': message.action,
+      'data': parameterMap,
+      'cache': true,
+      'dataType': 'jsonp',
+      'jsonpCallback': 'cb',
+      'success': function(data, textStats, XMLHttpRequest) {
+          gdata = data;
+          console.log(data);
+          //var output = prettyPrint(data);
+          //$("body").append(output);
+          
+          businesses = [];
 
-      _.each(gdata.businesses, function(business, index) {
-        b = new Business(business.name, business.image_url, business.snippet_text, business.rating_img_url);
-        businesses.push(b);
-      });
+          _.each(gdata.businesses, function(business, index) {
+            b = new Business(business.name, business.image_url, business.snippet_text, business.rating_img_url, business.url);
+            businesses.push(b);
+          });
 
-      console.log(b);
-      // writeToPage();
-      writePageHeader();
-      deleteContent();
-      writePagecontent();
-      random();
-      writeRandomcontent();
+          console.log(b);
+          // writeToPage();
+          writePageHeader();
+          deleteContent();
+          writePagecontent();
+          random();
+          writeRandomcontent();
 
   }
 });
 
 };
-
-function Business(name, image_url, snippet_text, rating_img_url) {
-  this.name = name;
-  this.image_url = image_url;
-  this.snippet_text = snippet_text;
-  this.rating_img_url = rating_img_url;
+//Pull select information from api
+function Business(name, image_url, snippet_text, rating_img_url, url) {
+    this.name = name;
+    this.image_url = image_url;
+    this.snippet_text = snippet_text;
+    this.rating_img_url = rating_img_url;
+    this.url = url;
 };
 
+//display the item and location
 function writePageHeader(){
-  $("#nearlist").html(near);
-  $("#termslist").html(terms);
+    $("#nearlist").html(near);
+    $("#termslist").html(terms);
 };
 
+//display the 10 results to page
 function writePagecontent(){
   for(i = 0; i <= 10; i++){
-  $(".list").append("<a href=\"#\" class=\"list-group-item \"><h4 class=\"list-group-item-heading name\">" + 
-    businesses[i].name + " " +
-    "<span class=\"rating\"><image_url:\"" + 
-    businesses[i].rating_img_url + 
-    "\"></span></h4> <p class=\"list-group-item-text snippit\">" + 
-    businesses[i].snippet_text + "</p></a>" );      
-}
+    $(".list").append("<div class=\"media\"><a class=\"pull-left\" href=" + businesses[i].url + "><img class=\"media-object\" src=" + 
+        businesses[i].image_url +  
+        "></a><div class=\"media-body\"><h4 class=\"media-heading\">" + 
+        businesses[i].name + 
+        " <span><img class=\"reviewImg\" src=" + 
+        businesses[i].rating_img_url + 
+        "></span></h4>" +
+        businesses[i].snippet_text +
+        "</div>");
+  }
 };
 
+
+
+// delete content after submit
 function deleteContent(){
-  $(".list").empty();
+    $(".list").empty();
 };
 
+//enter runs submit function
 function enter(){
-$("#near").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#submit").click();
-    }
-});
+  $("#near").keyup(function(event){
+      if(event.keyCode == 13){
+          $("#near").click();
+      }
+  });
 }
 
-
-
+//choose a random number and assign to businesses
 function random(){
-
-  var restaurantnumber = Math.floor(Math.random() * 10)
-
-    switch(restaurantnumber)
-    {
-    case 1:
-      radrest = businesses[1];
-      break;
-    case 2:
-      radrest = businesses[2];
-      break;
-    case 3:
-      radrest = businesses[3];
-      break;
-    case 4:
-      radrest = businesses[4];
-      break;
-    case 5:
-      radrest = businesses[5];
-      break;
-    case 6:
-      radrest = businesses[6];
-      break;
-    case 7:
-      radrest = businesses[7];
-      break;
-    case 8:
-      radrest = businesses[8];
-      break;
-    case 9:
-      radrest = businesses[9];
-      break;
-    case 10:
-      radrest = businesses[0];
-      break;
-    default:
-      radrest = businesses[0];
-      break;
-    }
-
+    var restaurantnumber = Math.floor(Math.random() * 10);
+    radrest = businesses[restaurantnumber];
 }
 
+//display random picture
 function writeRandomcontent(){
-  $(".random").html("<img src=" + radrest.image_url + " \"width=\"270px\" height=\"250px\">" + "<h4 class=\"list-group-item-heading\">" + radrest.name + "</h4>");
-     };
+    $(".random").html("<img src=" + radrest.image_url + " \"width=\"270px\" height=\"250px\">" + "<h4 class=\"list-group-item-heading\">" + radrest.name + "</h4>");
+};
 
-
+//random Button function
 $(document).ready(function(){
-    $( ".randomBottom" ).click(function() {
-      random();
-      writeRandomcontent();;
+    $( ".randomButton" ).click(function() {
+        random();
+        writeRandomcontent();;
     });
 });
 
